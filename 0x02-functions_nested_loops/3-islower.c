0a76124334fcddd6d43e2c12e50e286de5ddfc7b
Tiamiyu Abdussomad Olanrewaju <coder@Coder.Coder>
2022-09-13 21:55:52 +0100

    Is uppercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Prints all the lowercase alphabets ten times
+ * @c: character to be checked
+ * Return: void
+ */
+void _islower(int c)
+{
+	if (c < 97)
+		return (1);
+	else
+		return (0);
+}
