e570f07b6f6965fc92f9b41bb6972599853510b4
Coder <fiyiziyech@gmail.com>
2022-09-15 21:47:03 +0100

    Print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,16 @@ void print_number(int n)
 	if (n < 0)
 		_putchar('-');
 	n = abs(n);
-	if (n > 999)
+
+	if (n > 9999)
+	{
+		_putchar('0' + (n / 10000));
+		_putchar('0' + (n / 10000)/1000);
+		_putchar('0' + (n % 1000) / 100);
+		_putchar('0' + (n % 100) / 10);
+		_putchar('0' + (n % 10));
+	}
+	else if (n > 999)
 	{
 		_putchar('0' + (n / 1000));
 		_putchar('0' + (n % 1000) / 100);
