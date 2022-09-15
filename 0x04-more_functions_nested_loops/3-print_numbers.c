eac8ebdd3b99c5e1a4a456a0858ad5f05c69fe80
Coder <fiyiziyech@gmail.com>
2022-09-15 16:57:05 +0100

    Print all numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - Prints all digits
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	char i = '0';
+	while (i <= '9')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
