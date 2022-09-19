406412bc6d647ba20b11a7b21c5c4f93ed3895a1
Coder <fiyiziyech@gmail.com>
2022-09-19 23:13:36 +0100

    Prints half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - Prints the reverse of a string
+ * @str1: pointer to the string
+ * Return: nothing
+ */
+void puts_half(char *str1)
+{
+	int i, len, half;
+
+	len = _strlen(str1);
+	half = len / 2;
+	for (i = half; i < len; i++)
+	{
+		_putchar(str1[i]);
+	}
+	_putchar('\n');
+}
