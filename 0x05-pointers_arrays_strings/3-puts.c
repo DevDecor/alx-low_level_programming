cdf9b2811f2d89bffe09ee5f9a6348ecb7bccd9c
Coder <fiyiziyech@gmail.com>
2022-09-19 21:21:28 +0100

    Prints the string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @s: pointer to the string
+ * Return: the length of the string
+ */
+
+void _puts(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+}
