d35be9e6f60a2902b3b1dad9215b65456fa1674e
Coder <fiyiziyech@gmail.com>
2022-09-22 14:40:44 +0100

    String n copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies some bytes from the source pointer to the destination
+ * @dest: Destination pointer
+ * @src: Source pointer
+ * @n: Number of bytes to be copied
+ * Return: pointer tho the destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (src[i] == '\0')
+		dest[i] = src[i];
+	return (dest);
+}
