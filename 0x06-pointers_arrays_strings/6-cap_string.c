9c58f2a758dd20ffe99127b9da9e458dc80add7e
Coder <fiyiziyech@gmail.com>
2022-09-22 21:40:30 +0100

    Cap strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,45 +1,30 @@
 #include "main.h"
-
 /**
- * cap_string - Capitalizes each word of a string
- * @s: pointer to the string
- * Return: s
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
 char *cap_string(char *s)
 {
-	int i = 0;
-	int j;
-	int x[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	if (is_lower(s[i]) == 1)
-		s[i] == s[i] - 32;
-	while (s[i] != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		j = 0;
-		while (j < 13)
+		for (i = 0; i < 13; i++)
 		{
-			if (s[i] == x[j])
+			if (*(s + count) == sep_words[i])
 			{
-				if (is_lower(s[i + 1]) == 1)
-					s[i + 1] = s[i + 1] - 32;
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
 				break;
 			}
-			j++;
 		}
-		i++;
+		count++;
 	}
 	return (s);
-}
-
-/**
- * is_lower - checks if a string is in lower case
- * @p: string to be checked
- * Return: 1 or 0
- */
-int is_lower(char p)
-{
-	if (p >= 'a' && p <= 'z')
-		return (1);
-	else
-		return (0);
 }
