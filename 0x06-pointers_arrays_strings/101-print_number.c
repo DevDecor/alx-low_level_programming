ada7b199ab9f894d42a89232621568253e582470
Coder <fiyiziyech@gmail.com>
2022-09-25 22:42:17 +0100

    Print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -15,7 +15,7 @@ void print_number(int n)
 	if (n > 9999)
 	{
 		_putchar('0' + (n / 10000));
-		_putchar('0' + (n / 10000)/1000);
+		_putchar('0' + (n / 10000) / 1000);
 		_putchar('0' + (n % 1000) / 100);
 		_putchar('0' + (n % 100) / 10);
 		_putchar('0' + (n % 10));
