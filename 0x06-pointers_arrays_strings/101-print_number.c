47e20fbd709742063256f71c1d87524b9f5cc669
Coder <fiyiziyech@gmail.com>
2022-09-26 16:12:19 +0100

    Print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,7 +2,8 @@
 
 /**
  * print_number - prints an integer n
- *@n: integer to be printed
+ * @n: integer to be printed
+ * Return: nothing
  */
 void print_number(int n)
 {
@@ -12,32 +13,13 @@ void print_number(int n)
 		n = n * -1;
 	}
 
-	if (n > 9999)
+	if (n > 9)
 	{
-		_putchar('0' + (n / 10000));
-		_putchar('0' + (n / 10000) / 1000);
-		_putchar('0' + (n % 1000) / 100);
-		_putchar('0' + (n % 100) / 10);
-		_putchar('0' + (n % 10));
-	}
-	else if (n > 999)
-	{
-		_putchar('0' + (n / 1000));
-		_putchar('0' + (n % 1000) / 100);
-		_putchar('0' + (n % 100) / 10);
-		_putchar('0' + (n % 10));
-	}
-	else if (n > 99)
-	{
-		_putchar('0' + (n / 100));
-		_putchar('0' + (n % 100) / 10);
-		_putchar('0' + (n % 10));
-	}
-	else if (n > 9)
-	{
-		_putchar('0' + (n / 10));
-		_putchar('0' + (n % 10));
+		print_number(n / 10);
+		_putchar('0' + n % 10);
 	}
 	else
+	{
 		_putchar('0' + n);
+	}
 }
