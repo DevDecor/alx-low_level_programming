c061733b541c5f65438f3ceb9dca188b4dcf71b7
Coder <fiyiziyech@gmail.com>
2022-09-28 17:00:18 +0100

    find the string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "2-strchr.c"
+#include <stddef.h>
+char *_strstr(char *haystack, char *needle)
+{
+	char *p = _strchr(haystack, *(needle + 0));
+	int i;
+
+	for (i = 0; *(p + i) != '\0'; i++)
+	{
+		if (*(needle + i) == '\0')
+			return p;
+		if (*(p + i) == *(needle + i))
+			i++;
+		else
+		{
+			return _strstr(p + 1, needle);
+		}
+	}
+	return (NULL);
+}
