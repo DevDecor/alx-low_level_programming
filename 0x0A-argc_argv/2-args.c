78c70da8473520bfedbc210ca0420768f5f8901e
Coder <fiyiziyech@gmail.com>
2022-09-30 22:59:25 +0100

    Print all args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - The program
+ * @argc: command line arguments count
+ * @argv: command line arguments
+ * Return: The exit code
+ */
 int main(int argc, int *argv[])
 {
 	while (argc--)
-	printf("%s\n", *argv++);
+		printf("%s\n", *argv++);
 	return (0);
 }
