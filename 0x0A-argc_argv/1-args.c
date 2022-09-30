eb58931fba8e5d9f8368af1585f479dac83e6c0c
Coder <fiyiziyech@gmail.com>
2022-09-30 23:25:38 +0100

    Print all args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * @argc: argv count
+ * @argv: arguments
+ * Return: Exit code
+ */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
