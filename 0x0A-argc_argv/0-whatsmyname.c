864678e0f874e8994e35174692481f141d50ad59
Coder <fiyiziyech@gmail.com>
2022-09-30 23:20:56 +0100

    What is my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: the actual arguments
  * Return: Exit code
  */
-int main(int argc __attribute__((unused)), int *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
