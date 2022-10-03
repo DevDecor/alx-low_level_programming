b761e7e0ff3a68ad5096373ae7ea3de13b9217d5
Coder <fiyiziyech@gmail.com>
2022-10-03 20:30:25 +0100

    Duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,10 +12,12 @@ char *_strdup(char *str)
 	int i;
 	char *new;
 
-	if (str == NULL || new == NULL)
+	if (str == NULL)
 		return (NULL);
 	i = strlen(str);
 	new = malloc(sizeof(char) * (i + 1));
+	if (new == NULL)
+		return (NULL);
 	while (i > 0)
 	{
 		new[i] = str[i];
