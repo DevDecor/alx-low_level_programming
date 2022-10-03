2e4fe2dcb7c651030dc2a9ccd5155b74a7e51f27
Coder <fiyiziyech@gmail.com>
2022-10-03 19:47:55 +0100

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *str = malloc(sizeof(c) * size);
 
 	if (size == 0 || str == NULL)
-		return NULL;
+		return (NULL);
 	while (size != 0)
 	{
 		str[size] = c;
