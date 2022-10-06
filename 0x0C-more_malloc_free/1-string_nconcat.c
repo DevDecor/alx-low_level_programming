be2dc397d34209d8063712a5f9ff6a17d083ea44
Coder <fiyiziyech@gmail.com>
2022-10-06 22:21:54 +0100

    Concatenates n bytes of a string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,10 +1,24 @@
 #include "main.h"
 #include <string.h>
+
+/**
+ * string_nconcat - Concatenates n bytes of a string to another
+ * @s1: String one
+ * @s2: string two
+ * @n: number of bytes to be copied
+ * Return: Pointer to new string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int x = strlen(s1) + n + 1, i = 0;
- 	unsigned int j = 0;
-	char *res = malloc(x);
+	unsigned int j = 0;
+	char *res;
+
+	if (s1 != NULL)
+		x = strlen(s1) + n;
+	else
+		x = n;
+	res = malloc(x);
 
 	if (res == NULL)
 		return (NULL);
