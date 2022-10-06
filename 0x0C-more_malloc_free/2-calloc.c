ef571476c50617a9a24137d7898fe7c32803c14b
Coder <fiyiziyech@gmail.com>
2022-10-06 23:27:11 +0100

    Concatenates n bytes of a string

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,10 +14,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
-	if (p != NULL)
+	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		*(p + i) = 0;
+		p[i] = 0;
 
 	return (p);
 }
