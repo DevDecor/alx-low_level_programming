a6d1e27898f8001f76baf6074c471ba91cbd9cf0
Coder <fiyiziyech@gmail.com>
2022-10-10 22:40:22 +0100

    Creating a dog from  Dog structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,11 +1,17 @@
 #include "dog.h"
 #include "1-init_dog.c"
+/**
+ * new_dog - Create a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ * Return: pointer to the struct
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t d;
-	dog_t *p = &d;
-	init_dog(p, name, age, owner);
+	dog_t *p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
+	init_dog(p, name, age, owner);
 	return (p);
 }
