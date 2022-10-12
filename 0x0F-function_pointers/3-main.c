fe78ca2be80be0295b93f836e2f289c852e71002
Coder <fiyiziyech@gmail.com>
2022-10-12 17:39:33 +0100

    Math calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: arguments
+ * Return: status code
+ */
+int main(int argc, char *argv[])
+{
+	int (*p)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	p = get_op_func(argv[2]);
+
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '%' || argv[2][0] == '/') && (atoi(argv[3]) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", p(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a+b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Finds the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a-b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: a*b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two integer
+ * @a: first integer
+ * @b: second integer
+ * Return: a/b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Finds the modulus of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
