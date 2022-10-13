babf7d6fe57eb2339450ac86ba05c9cfb3edee18
Coder <fiyiziyech@gmail.com>
2022-10-13 15:53:48 +0100

    Prints all string arguments

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,11 +10,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list p;
+	char *sp;
 
 	va_start(p, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(p, char *));
+		sp = va_arg(p, char *);
+		if (sp == NULL)
+			printf("(nil)");
+		else
+			printf("%s", sp);
 		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
