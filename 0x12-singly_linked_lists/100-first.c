e854599735742b99e3114fa650fada2aef621879
Coder <fiyiziyech@gmail.com>
2022-10-21 00:23:51 +0100

    Prints before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 /**
- * myStartupFun - runs before main
+ * __attribute__ - runs before main
+ * @myStartupFun: mmmmfdfkd
  * Return: nothing
  */
 void (__attribute__ ((constructor)) myStartupFun)(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
