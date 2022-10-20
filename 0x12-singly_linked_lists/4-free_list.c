2ce2dd6481b4256e90c33a259c51db7d60fcb3de
Coder <fiyiziyech@gmail.com>
2022-10-20 23:37:03 +0100

    Free node

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - Frees the allocated memories for a linked list
+ * @head: the head node of the list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *this;
+
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		this = head;
+		head = head->next;
+		free(this);
+	}
+	free(head);
+}
