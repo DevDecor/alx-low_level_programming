e68a22b21e2d40b24e029eaeb4bb47e181bafa4b
Coder <fiyiziyech@gmail.com>
2022-10-25 22:04:48 +0100

    Frees a list int

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint
+ * @head: the head node of the list
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+
+	if (*head == NULL)
+		return;
+	while ((*head)->next != NULL)
+	{
+		p = *head;
+		(*head) = (*head)->next;
+		free(p);
+	}
+	free(*head);
+	(*head) = NULL;
+}
