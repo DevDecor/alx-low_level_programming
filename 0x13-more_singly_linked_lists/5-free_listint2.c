9a4a06933e9aa37a939092c9b3e3cdd724c85747
Coder <fiyiziyech@gmail.com>
2022-10-25 22:13:42 +0100

    Frees a list int

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,14 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *p;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 	while ((*head) != NULL)
 	{
 		p = *head;
 		*head = p->next;
 		free(p);
-		p = NULL;
 	}
 	*head = NULL;
 }
