51f03c2255066c7ae9961e661ecdbabd93858ebe
Coder <fiyiziyech@gmail.com>
2022-10-25 20:51:12 +0100

    Print list int

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * print_listint - Prints linked list of integers
+ * @h: head node
+ * Return: length of the list
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t c = 0;
