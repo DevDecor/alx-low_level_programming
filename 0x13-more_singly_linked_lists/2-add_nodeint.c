07329a6f656147560f88b6ab373eb18ea209fc59
Coder <fiyiziyech@gmail.com>
2022-10-25 21:25:05 +0100

    Prepend to the list int

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,6 +9,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *p = malloc(sizeof(listint_t));
 
+	if (p == NULL)
+		return (NULL);
 	p->n = n;
 	p->next = NULL;
 	if (*head == NULL)
