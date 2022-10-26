62e71b1a38f2005029bd3fa139813e67d9132203
Coder <fiyiziyech@gmail.com>
2022-10-26 04:12:41 +0100

    Indexes a list int

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 #include "1-listint_len.c"
+/**
+ * get_nodeint_at_index - Gets the node at specific index
+ * @head: head node of the list
+ * @index: required index
+ * Return: List the node at index
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int x = 0;
