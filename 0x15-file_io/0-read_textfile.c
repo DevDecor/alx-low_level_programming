9d76a154a87e10c39f0a365a1f236f25f70d2870
Coder <fiyiziyech@gmail.com>
2022-11-01 16:57:12 +0100

    Reads a text file and write to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,14 +1,20 @@
 #include "main.h"
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buff = malloc(letters + 1);
+	char *buff = malloc(sizeof(char) * letters);
 	int count, op = open(filename, O_RDONLY);
 
 	if (buff == NULL || filename == NULL || op < 0)
+	{
+		free(buff);
 		return (0);
+	}
 
 	count = read(op, buff, letters);
+	if (count == -1)
+		return (0);
 	buff[letters] = '\0';
-	write(0, buff, count);
+	write(STDOUT_FILENO, buff, count);
+	free(buff);
 	return (count);
 }
