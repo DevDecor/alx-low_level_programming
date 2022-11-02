8a1483f0095e992fd03796fb561f0a9277d14d51
Coder <fiyiziyech@gmail.com>
2022-11-02 21:53:07 +0100

    x qRevert "Reads a text file and write to stdout"
    This reverts commit 73ef0e731749a1885fd9e5a71099a5b4431be90f.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,22 +1,25 @@
 #include "main.h"
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char buff[];
+	char *buff = malloc(sizeof(char) * letters);
 	ssize_t count, op = open(filename, O_RDONLY), x;
 
 	if (buff == NULL || filename == NULL || op < 0)
 	{
+		free(buff);
 		return (0);
 	}
 
 	count = read(op, buff, letters);
 	if (count == -1)
 	{
+		free(buff);
 		return (0);
 	}
 	buff[letters] = '\0';
 	x = write(STDOUT_FILENO, buff, count);
 	if (x == -1 || x != count)
 		return (0);
+	free(buff);
 	return (x);
 }
