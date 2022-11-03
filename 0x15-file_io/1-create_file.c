e52b3aa55b5434d98eefde090af83878046ff718
Coder <fiyiziyech@gmail.com>
2022-11-03 10:44:08 +0100

    Writes or modifies a text file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ * create_file - Creates and write a file
+ * @filename: name of the file to be created
+ * @text_content: content to be written
+ * Return: 1 on success otherwise -1
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int file, x = 0;
@@ -11,5 +17,6 @@ int create_file(const char *filename, char *text_content)
 	x = write(file, text_content, strlen(text_content));
 	if (x == -1)
 		return (-1);
+	close(file);
 	return (1);
 }
