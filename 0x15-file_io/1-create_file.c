cfe4a7699fd8a8fa2728ec39528ae75498a859d5
Coder <fiyiziyech@gmail.com>
2022-11-03 10:54:59 +0100

    Writes or modifies a text file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,11 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
 	if (file == -1)
 		return (-1);
-	x = write(file, text_content, strlen(text_content));
+
+	if (text_content)
+	{
+		x = write(file, text_content, strlen(text_content));
+	}
 	if (x == -1)
 		return (-1);
 	close(file);
