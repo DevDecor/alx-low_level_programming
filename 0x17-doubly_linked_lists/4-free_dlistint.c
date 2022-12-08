734c1f5084c86b273a56feb513eadd09c2c73639
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2022-12-08 17:42:19 +0100

    Free doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dnodeint - Adds a node at the beginning of a doubly linked list
+ * @head: head node
+ * Return: the new head node
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *b = head;
+
+	while (head != NULL)
+	{
+		b = head->next;
+		free(head);
+		head = b;
+	}
+}
