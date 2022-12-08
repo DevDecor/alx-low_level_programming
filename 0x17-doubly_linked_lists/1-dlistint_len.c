ddf8650eb2bd5d1c3cb9936b9f6c7a141a410e6b
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2022-12-08 13:52:23 +0100

    Count doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * @h: head node of the list
  * Return: length of the list
  */
-size_t dlistint_len(const dlistint_t *h)
+size_t dlistint_len(dlistint_t *h)
 {
 	size_t i = 0;
 	dlistint_t *b = h;
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -14,6 +14,6 @@ typedef struct dlistint
 	struct dlistint *next;
 	struct dlistint *prev;
 } dlistint_t;
-size_t dlistint_len(const dlistint_t *h);
+size_t dlistint_len(dlistint_t *h);
 size_t print_dlistint(dlistint_t *h);
 #endif
