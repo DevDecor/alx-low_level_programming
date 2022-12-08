9b5f6ce23c727b4379146ebeab5990770f84def3
Tiamiyu Abdussomad Olanrewaju <coder@coder.com>
2022-12-08 21:42:43 +0100

    Adds a node at an index of a doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,27 +6,29 @@
  * @n: number to be inserted
  * Return: the new head node
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t *head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 {
-	dlistint_t *b = head, *s = malloc(sizeof(dlistint_t));
+	dlistint_t *b = *head, *s = malloc(sizeof(dlistint_t));
 	unsigned int i = 0;
 
 	if (s == NULL)
 		return (NULL);
 	s->n = n;
-	if (b == NULL)
-		return (head = s);
 	while (b != NULL)
 	{
-		if (i == index)
+		if (i == idx)
 		{
-			s->next = b->next;
-			s->prev = b;
-			b->next = s;
+			if (i == 0)
+				*head = s;
+			s->next = b;
+			s->prev = b->prev;
+			b->prev->next = s;
+			b->prev = s;
 			return (s);
 		}
 		b = b->next;
 		i++;
 	}
+	free(s);
 	return (NULL);
 }
