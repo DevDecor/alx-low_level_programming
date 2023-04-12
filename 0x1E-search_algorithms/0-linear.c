4e92558402d9575c69ffcf69c1a21152b15e647f
DevLarry <olanrewajusamad070@gmail.com>
2023-04-12 13:01:02 +0100

    Linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,18 @@
+/**
+ * linear_search - Implimenting the linear search algorithm
+ * @array: the array to search
+ * @size: length of the array
+ * @value: value to be searched
+ * Return: index of the found element of -1 if not found
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
