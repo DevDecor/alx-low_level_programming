cd00dde551a288e5d91c92b103d4ad0e6649aa5d
DevDecor <fiyiziyech@gmail.com>
2023-07-13 00:37:22 +0100

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,30 +1,34 @@
 #include <math.h>
 #include "search_algos.h"
+/**
+ * jump_search - Searches an array of integers using
+ * the jump search algorithm
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: valuse to be searched
+ * Return: index of the match
+ */
 int jump_search(int *array, size_t size, int value)
 {
-	int start = 0, jump = sqrt(size), end = jump - 1, i;
+	size_t start = 0, jump = (int) sqrt(size), end = jump, i;
 
-	if (!array || size < 1) return (-1);
-	while ((end < (int) size) && end != start)
+	if (!array || size < 1)
+		return (-1);
+	while (end <= size - 1 && array[end] < value)
 	{
-		if (array[end] <= value)
-		printf("Value checked array[%d] = [%d]\n", start, array[start]);
-		if (array[end] < value)
-		{
-			start = end + 1;
-			end = start + jump;
-		}
-		else if (array[end] == value) return (end);
-		else
-		{
-		printf("Value found between indexes [%d] and [%d]\n", start, end);
-			for (i = start; i <= end; i++)
-			{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
-			if (array[i] == value) return (i);
-			}
-			break;
-		}
+		printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+		start = end;
+		end = start + jump;
+	}
+	printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+	printf("Value found between indexes [%ld] and [%ld]\n", start, end);
+	if (end >= size)
+		end = size - 1;
+	for (i = start; i <= end; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
 }
